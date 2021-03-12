3483269e91b0398ef8dce66643b245634a4cc299
Eli Kaplan <elikaplan@me.com>
2021-03-12 01:30:35 +0000

    no new logic, just some eslint fixes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,16 @@
-const request = require('request')
+const request = require('request');
 
-const breed = process.argv[2]
+const breed = process.argv[2];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if(error !== null){
-  console.log(`error:`, error)
-  }
-  else{
-const data = JSON.parse(body)
-if(data[0] === undefined){
-  console.log(`The breed you're looking for doesn't exist! Perhaps it was misspelled?`)
-}
-else{
-console.log(data[0].description)
-}
+  if (error !== null) {
+    console.log(`error:`, error);
+  } else {
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      console.log(`The breed you're looking for doesn't exist! Perhaps it was misspelled?`);
+    } else {
+      console.log(data[0].description);
+    }
   }
 });
